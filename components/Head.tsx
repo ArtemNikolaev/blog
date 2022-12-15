d43a83a6629a0998f3257ee59ca7f1c5e07ffa8e
Artem Nikolaev <artem@nikolaev.by>
2022-12-16 00:05:21 +0300

    fix: naming

--- components/Head.tsx
+++ components/Head.tsx
@@ -23,7 +23,7 @@ const Head = ({ customMeta }: { customMeta?: MetaProps }): JSX.Element => {
       <meta property="og:url" content={`${WEBSITE_HOST_URL}${router.asPath}`} />
       <link rel="canonical" href={`${WEBSITE_HOST_URL}${router.asPath}`} />
       <meta property="og:type" content={meta.type} />
-      <meta property="og:site_name" content="Hunter Chang - Website" />
+      <meta property="og:site_name" content="Artem Nikolaev - Website" />
       <meta property="og:description" content={meta.description} />
       <meta property="og:title" content={meta.title} />
       <meta property="og:image" content={meta.image} />
